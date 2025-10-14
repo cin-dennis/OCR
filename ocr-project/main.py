7c212fc5ae31a8e91bf69ea66f0987299fbc688c
cin-dennis <dennis@cinnamon.is>
2025-10-14 14:01:21 +0700

    chore: setup source root

--- ocr-project/main.py
+++ ocr-project/main.py
@@ -4,7 +4,3 @@ from app.api.endpoints import files
 app = FastAPI(title="OCR Processing System")
 
 app.include_router(files.router, prefix="/api/v1", tags=["File Upload"])
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
